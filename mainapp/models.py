fc43006eb3da3f45c41a23740321bdfeff516790
alim <mamutov.creation@yandex.ru>
2022-04-13 15:56:55 +0300

    1

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.db import models
 
 # Create your models here.
@@ -5,6 +7,7 @@ from authapp.models import User
 
 
 class Project(models.Model):
+    # uid = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(max_length=255, verbose_name="Название проекта")
     repository = models.URLField(max_length=128, blank=True, verbose_name='Ссылка на репозиторий')
     users = models.ManyToManyField(User, blank=True, verbose_name='Пользователи')
