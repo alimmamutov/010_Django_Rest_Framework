36e80f018b242482f3399a2565987cd7ad76ddb4
alim <mamutov.creation@yandex.ru>
2022-04-13 15:52:21 +0300

    1

--- service/settings.py
+++ service/settings.py
@@ -133,9 +133,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.AdminRenderer',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': [
+#         'rest_framework.renderers.JSONRenderer',
+#         'rest_framework.renderers.AdminRenderer',
+#     ]
+# }
