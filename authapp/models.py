ef66b5a9c327f19e0506a7c7cfdb7c585557110a
alim <mamutov.creation@yandex.ru>
2022-04-14 12:34:48 +0300

    1

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
@@ -5,6 +7,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(verbose_name='email', unique=True, db_index=True)
+    id = models.UUIDField(primary_key=True, default=uuid4)
 
     def __str__(self):
         return f'{self.username} {self.first_name} {self.last_name}'
